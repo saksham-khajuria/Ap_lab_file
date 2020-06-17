5107a950350bce3b18dd76c9028da3a92b1d4dba
saksham-khajuria <54791574+saksham-khajuria@users.noreply.github.com>
2020-06-17 10:56:51 +0530

    Add files via upload

---
+++ 6. Creating Websites using PHP/contact.php
@@ -0,0 +1,3 @@
+<?php echo file_get_contents("header.html");?>
+<?php echo file_get_contents("navigation.html");?>
+<?php echo file_get_contents("abt.html");?>
---
+++ 6. Creating Websites using PHP/index.php
@@ -0,0 +1,4 @@
+<?php echo file_get_contents("header.html");?>
+<?php echo file_get_contents("navigation.html");?>
+<?php echo file_get_contents("body.html");?>
+<?php echo file_get_contents("footer.html");?>
